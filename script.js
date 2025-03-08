0fd2fd928fc2b5587126c2ea31c1d66b5693d6bd
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-03-08 22:32:52 +0530

    implement game logic for Rock Paper Scissors: add functions for computer and human choices

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// Step 1: Setup the project structure
 console.log("Hello there, We're gonna play # Project: Rock Paper Scissors soon!")
+
+// Step 2: Write the logic to get the computer choice
+function getComputerChoice(){
+    let randomNumber=Math.floor(Math.random()*3)
+
+    if(randomNumber===0){
+        return "Rock"
+    }else if(randomNumber===1){
+        return "Paper"
+    }else{
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
+
+// Step 3: Write the logic to get the human choice
+function getHumanChoice(){
+    let userInput=prompt("Rock, Paper, or Scissors ?")
+    console.log(userInput)
+}
+
+getHumanChoice()
+
+// Step 4: Declare the players score variables
+
+// Step 5: Write the logic to play a single round
+
+// Step 6: Write the logic to play the entire game
