1717aef84866ef4db3a837bffdb7f137333c7cfe
Muzammil Ibrahim <muzammilibrahim13@gmail.com>
2025-03-09 22:21:53 +0530

    add game rules and implement playGame function for Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Step 1: Setup the project structure
 console.log("Hello there, We're gonna play # Project: Rock Paper Scissors soon!")
+let gameRules=["Rock beats Scissors",
+                "Scissors beats Paper",
+                "Paper beats Rock"]
+console.log("Game rules: "+gameRules)
 
 // Step 2: Write the logic to get the computer choice
 function getComputerChoice(){
@@ -9,64 +13,79 @@ function getComputerChoice(){
         return "Rock"
     }else if(randomNumber===1){
         return "Paper"
-    }else if(randomNumber===2){
-        return "Scissors"
     }else{
-        return "Tie"
+        return "Scissors"
     }
 }
-
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
 
 // Step 3: Write the logic to get the human choice
 function getHumanChoice(){
     let userInput=prompt("Rock, Paper, or Scissors ?")
     console.log(userInput)
+    return userInput;
 }
-
-getHumanChoice()
+// getHumanChoice()
 
 // Step 4: Declare the players score variables
-// let humanScore=0;
-// let computerScore=0;
+let humanScore=0;
+let computerScore=0;
 
 // Step 5: Write the logic to play a single round
-// function playRound(humanChoice,computerChoice){
-//        humanChoice=humanChoice.toLowerCase()
+function playRound(humanChoice,computerChoice){
+       humanChoice=humanChoice.toLowerCase();
+       computerChoice=computerChoice.toLowerCase();
 
-//        console.log("You lose! Paper beats Rock")
+       console.log(`Human: ${humanChoice} | Computer: ${computerChoice}`)
 
-//        if(humanChoice>computerChoice){
-//         humanScore+=1;
-//        }else if(computerChoice>humanChoice){
-//         computerScore+=1;
-//        }else{
-//         pass
-//        }
+       if(
+        (humanChoice==="rock" && computerChoice==="scissors") ||
+        (humanChoice==="scissors" && computerChoice==="paper") ||
+        (humanChoice==="paper" && computerChoice==="rock")
+       ){
+        humanScore+=1
+        console.log(`You Scored! ${humanChoice} beats ${computerChoice}`)
+       }else if(humanChoice===computerChoice){
+        console.log(`Its a Tie! You both chose ${humanChoice}`)
+       }else{
+        computerScore+1
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+       }
 
-// }
+    console.log(`Score: Computer=${computerScore} | Human=${humanScore}`)
+}
 
-const humanSelection=getHumanChoice();
-const computerSelection=getComputerChoice();
+// const humanSelection=getHumanChoice();
+// const computerSelection=getComputerChoice();
 
-playRound(humanSelection,computerSelection)
+// playRound(humanSelection,computerSelection)
 
 // Step 6: Write the logic to play the entire game
 function playGame(){
+    humanScore=0;
+    computerScore=0;
 
-    function playRound(humanChoice,computerChoice){
-        humanChoice=humanChoice.toLowerCase()
- 
-        console.log("You lose! Paper beats Rock")
- 
-        if(humanChoice>computerChoice){
-         humanScore+=1;
-        }else if(computerChoice>humanChoice){
-         computerScore+=1;
-        }else{
-         return "Tie"
-        }
- }
-    let humanScore=0;
-    let computerScore=0;
+// Play 5 rounds by calling playRound 5 times.
+    for(let i=0;i<5;i++){
+        let humanSelection=getHumanChoice();
+        let computerSelection=getComputerChoice();
+        playRound(humanSelection,computerSelection)
+    }
+
+    console.log("Game OVER!")
+    console.log(
+        humanScore>computerScore
+        ?"You Win the match"
+        :humanScore<computerScore
+        ?"Computer Wins the match!"
+        :"WE GOT TIE!"
+    )
+}
+
+// start the game
+// playGame()
+if(confirm("Do you want to play?")){
+    playGame()
+}else{
+    console.log("May be next time!")
 }
